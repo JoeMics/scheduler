d64aa9010a6b33c8bf3cc744c40446877757d179
Joseph Micla <josephmicla@gmail.com>
2021-12-08 14:11:55 -0800

    adds class names conditionally to InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,12 +1,18 @@
 import React from "react";
+import classNames from "classnames";
+
 import 'components/InterviewerListItem.scss'
 
 const InterviewerListItem = (props) => {
-  const { id, name, avatar, setInterviewer } = props;
+  const { id, name, avatar, setInterviewer, selected } = props;
 
+  const interviewerItemClass = classNames({
+    "interviewers__item" : true,
+    "interviewers__item--selected": selected,
+  });
 
   return (
-    <li className="interviewers__item" onClick={() => setInterviewer(id)}>
+    <li className={interviewerItemClass} onClick={() => setInterviewer(id)} selected={selected}>
       <img
         className="interviewers__item-image"
         src={avatar}
