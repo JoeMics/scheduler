688b816a88b073d4e10bbbb09ca11fda221f4a57
Joseph Micla <josephmicla@gmail.com>
2021-12-08 14:05:43 -0800

    adds data dynamically to InterviewerListItem

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 import 'components/InterviewerListItem.scss'
 
-const InterviewerListItem = () => {
+const InterviewerListItem = (props) => {
+  const { id, name, avatar, setInterviewer } = props;
+
+
   return (
-    <li className="interviewers__item">
+    <li className="interviewers__item" onClick={() => setInterviewer(id)}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={avatar}
+        alt={name}
       />
-      Sylvia Palmer
+      {name}
     </li>
   );
 };
