6eea7a641535ab070748b057debcf12c821fdd4a
Joseph Micla <josephmicla@gmail.com>
2021-12-08 09:57:41 -0800

    adds useState to keep track of day state

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import DayList from "./DayList";
 
 import "components/Application.scss";
@@ -21,7 +22,9 @@ const days = [
   },
 ];
 
-export default function Application(props) {
+const Application = (props) => {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,8 +37,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList 
             days={days} 
-            day={'Monday'} 
-            setDay={(day) => console.log(day)}
+            day={day} 
+            setDay={setDay}
           />
         </nav>
         <img
@@ -50,3 +53,5 @@ export default function Application(props) {
     </main>
   );
 }
+
+export default Application;
