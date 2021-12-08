4d435e9d09c0af51350203892476af77b5ba5d7a
Joseph Micla <josephmicla@gmail.com>
2021-12-07 16:54:03 -0800

    creates DayList component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,9 +2,23 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 const DayList = (props) => {
+  const { days } = props;
+
+  const dayListItems = days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
+
   return (
     <ul>
-
+      {dayListItems}
     </ul>
   );
 };
