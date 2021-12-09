7f68da31a167b64e89be398f157ef57cb196ff50
Joseph Micla <josephmicla@gmail.com>
2021-12-08 18:29:36 -0800

    creates and exports Confirm component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,23 @@
+import Button from "components/Button";
+import React from "react";
+
+const Confirm = (props) => {
+  /* props
+  message: String, ex. "Are you sure you want to delete?"
+  onConfirm: Function, when confirm clicked
+  onCancel: Function, when cancel clicked
+   */
+  const { message, onConfirm, onCancel } = props;
+
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">{message}</h1>
+      <section className="appointment__actions">
+        <Button onClick={onCancel} danger>Cancel</Button>
+        <Button onClick={onConfirm} danger>Confirm</Button>
+      </section>
+    </main>
+  );
+};
+
+export default Confirm;
