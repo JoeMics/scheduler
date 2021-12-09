70525c2902ce795c74515475c26c670f499364a0
Joseph Micla <josephmicla@gmail.com>
2021-12-08 18:24:03 -0800

    creates story for Show component

--- stories/index.js
+++ stories/index.js
@@ -160,4 +160,11 @@ storiesOf("Button", module)
           // Appointment - Empty
           .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
           // Appointment - Show
-          .add("Show", () => <Show />)
+          .add("Show", () => { 
+            return <Show 
+            student="Lydia Miller-Jones"
+            interviewer={interviewers[0]}
+            onEdit={action("onEdit")}
+            onDelete={action("onDelete")}
+            />;
+          })
