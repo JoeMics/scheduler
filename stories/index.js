a8bb918f3b73180aa39e76c01d896dcdf8ea5036
Joseph Micla <josephmicla@gmail.com>
2021-12-08 17:58:01 -0800

    creates Empty story in stories

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // Button
 
@@ -155,4 +156,4 @@ storiesOf("Button", module)
           .add("Appointment with Time", () => <Appointment time="12pm"/>)
           // Appointment - Header
           .add("Header", () => <Header time="12pm"/>)
-
+          .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
