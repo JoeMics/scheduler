cb71217b9528118eb485d20d315c2e37438b31f4
Joseph Micla <josephmicla@gmail.com>
2021-12-08 18:49:16 -0800

    creates Error component

---
+++ src/components/Appointment/Error.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Error = (props) => {
+  /* props
+  message: String, ex. "Could not delete appointment"
+  onClose: Function, when close button clicked
+   */
+  const { message, onClose } = props;
+
+  return (
+    <main className="appointment__card appointment__card--error">
+      <section className="appointment__error-message">
+        <h1 className="text--semi-bold">Error</h1>
+        <h3 className="text--light">{message}</h3>
+      </section>
+      <img
+        className="appointment__error-close"
+        src="images/close.png"
+        alt="Close"
+        onClick={onClose}
+      />
+    </main>
+  );
+};
+
+export default Error;
