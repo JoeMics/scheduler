4f9843bd3ea1449230e57d516d1a2df2ae1bfc5c
Joseph Micla <josephmicla@gmail.com>
2021-12-08 17:49:26 -0800

    creates story for Appointment/Header, takes in time as props

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const Header = () => {
+const Header = (props) => {
+  /* props
+  time: String, time of appointment ex. 12pm
+   */
+  const { time } = props;
+  
   return (
     <header className="appointment__time">
-      <h4 className="text--semi-bold">12pm</h4>
+      <h4 className="text--semi-bold">{time}</h4>
       <hr className="appointment__separator" />
     </header>
   );
