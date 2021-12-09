c7cf2d01ea983eee29bf627cd963295877587470
Joseph Micla <josephmicla@gmail.com>
2021-12-08 21:00:24 -0800

    maps over appointments to render Appointments components

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import DayList from "./DayList";
 
 import "components/Application.scss";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -22,8 +23,51 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+  }
+];
+
 const Application = (props) => {
   const [day, setDay] = useState('Monday');
+
+  const renderedAppointmentsArray = appointments.map(appointment => (
+    <Appointment {...appointment} key={appointment.id} />
+  ));
 
   return (
     <main className="layout">
@@ -48,7 +92,8 @@ const Application = (props) => {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {renderedAppointmentsArray}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
