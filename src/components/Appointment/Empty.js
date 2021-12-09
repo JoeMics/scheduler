4cf56cdc813c6f86c60483055ad1947e73467511
Joseph Micla <josephmicla@gmail.com>
2021-12-08 18:00:59 -0800

    gives onAdd prop to Empty component

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-const Empty = () => {
+const Empty = (props) => {
+  /* props
+  onAdd: function */
+  const { onAdd } = props;
+
   return (
     <main className="appointment__add">
       <img
         className="appointment__add-button"
         src="images/add.png"
         alt="Add"
+        onClick={onAdd}
       />
     </main>
   );
