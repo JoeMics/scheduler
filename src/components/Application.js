b40a987aaaea5722ed8cb65d286e0d60fb8e15fc
Joseph Micla <josephmicla@gmail.com>
2021-12-09 16:34:05 -0800

    installs axios and imports into App

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 import "components/Application.scss";
-import Appointment from "./Appointment";
 
 const days = [
   {
