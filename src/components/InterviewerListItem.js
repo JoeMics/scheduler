cec83bb6e19e3b8e6d507cb0fe268d23896c8204
Joseph Micla <josephmicla@gmail.com>
2021-12-09 22:06:09 -0800

    adds forced semicolon to eslintrc.json

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 
-import 'components/InterviewerListItem.scss'
+import 'components/InterviewerListItem.scss';
 
 const InterviewerListItem = (props) => {
   /* 
