b26fd5737d40d5e2096c8d0fae079f459aebc523
Joseph Micla <josephmicla@gmail.com>
2021-12-09 17:31:20 -0800

    creates helper selector getAppointmentsForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,3 +1,16 @@
-export const getAppointmentsForDay = () => {
-  
+/*
+ * input: Object: state, String: day
+ *  
+ * output: Array of appointments for day
+ */
+export const getAppointmentsForDay = (state, day) => {
+  const selectedDay = state.days.find(appointment => appointment.name === day);
+
+  if (!selectedDay) {
+    return [];
+  }
+
+  return selectedDay.appointments.map(appointmentId => {
+    return state.appointments[appointmentId];
+  });
 };
