fcc135773dd98c1c2198f460ff6fc744f9e02811
Joseph Micla <josephmicla@gmail.com>
2021-12-10 17:37:25 -0800

    adds interviewers array to days objects

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -16,6 +16,23 @@ export const getAppointmentsForDay = (state, day) => {
 };
 
 /*
+ * input: Object: state, String: day
+ *  
+ * output: Array of appointments for day
+ */
+export const getInterviewersForDay = (state, day) => {
+  const selectedDay = state.days.find(appointment => appointment.name === day);
+
+  if (!selectedDay) {
+    return [];
+  }
+
+  return selectedDay.appointments.map(appointmentId => {
+    return state.appointments[appointmentId];
+  });
+};
+
+/*
  * input: Object: state, Object: interview
  *  
  * output: Object { student, interviewer: { id, name, avatar}}
