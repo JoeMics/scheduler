8706e04787592f2854fedd6809b83422ca923529
Joseph Micla <josephmicla@gmail.com>
2021-12-10 16:49:54 -0800

    imports useVisualMode into Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
 
 import 'components/Appointment/styles.scss';
 
@@ -10,9 +11,14 @@ const Appointment = (props) => {
   time: String ex. "4pm" 
   interview: Object
   */
-
+ 
   const { time , interview } = props;
+ 
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
 
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+  
   return (
     <article className="appointment">
       <Header time={time}/>
