4c15406fe3adda685181c75feed20171db31343a
Joseph Micla <josephmicla@gmail.com>
2021-12-13 21:09:18 -0800

    adds Confirm component before deleting appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,12 +7,14 @@ import useVisualMode from "hooks/useVisualMode";
 
 import "components/Appointment/styles.scss";
 import Status from "./Status";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
 const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 const Appointment = (props) => {
   /* props
@@ -60,7 +62,9 @@ const Appointment = (props) => {
     <article className="appointment">
       <Header time={time} />
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-      {mode === SHOW && <Show {...interview} onDelete={deleteAppointment} />}
+      {mode === SHOW && (
+        <Show {...interview} onDelete={() => transition(CONFIRM)} />
+      )}
       {mode === CREATE && (
         <Form
           interviewers={interviewers}
@@ -70,6 +74,13 @@ const Appointment = (props) => {
       )}
       {mode === SAVING && <Status message="Saving" />}
       {mode === DELETING && <Status message="Deleting" />}
+      {mode === CONFIRM && (
+        <Confirm
+          message="Are you sure you want to delete?"
+          onCancel={() => back()}
+          onConfirm={deleteAppointment}
+        />
+      )}
     </article>
   );
 };
